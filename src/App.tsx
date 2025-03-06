15b6e60ed1f598722f565144705cdf9bdb299d06
Yuki <146095835+roshireku@users.noreply.github.com>
2025-03-06 13:10:07 +0000

    5

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import viteLogo from "/vite.svg";
 
 export const App = () => {
   const [count, setCount] = useState(0);
-  //const [title, setTitle] = useState("ROCKS");
+  const [titolo, setTitle] = useState("Frontend Rocks");
 
   return (
     <div className="h-dvh flex flex-col items-center justify-center">
       <div className="bg-white p-8 rounded-md shadow-lg">
         <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          ROCKS
+          {titolo}
         </h1>
 
         <div className="flex justify-center space-x-4 mb-2">
@@ -22,21 +22,23 @@ export const App = () => {
           </a>
         </div>
 
-        <h2 className="text-center font-bold text-xl mb-6">1 REACT = 1€</h2>
+        <h2 className="text-center font-bold text-xl mb-6">1 CLICK = 1€</h2>
 
         <div className="flex flex-col items-center space-y-4">
           <button
             className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
             onClick={() => setCount((count) => count + 1)}
           >
-             Hai guadagnato {count}€
+            hai ottenuto {count}€
           </button>
+
           <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-red-600 transition-colors"
-         // onClick={() => setTitle("MAKE MONEY")}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
+              onClick={() => setTitle("Charizard")}
           >
-            cambia titolo
+            Cambia
           </button>
+
           <p className="text-center">
             Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
           </p>
