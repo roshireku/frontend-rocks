7b61558c1e4440f4bd0561bd577203a9db26e500
Yuki <146095835+roshireku@users.noreply.github.com>
2025-03-06 11:03:58 +0000

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import viteLogo from "/vite.svg";
 
 export const App = () => {
   const [count, setCount] = useState(0);
+  const [title, setTitle] = useState("ROCKS");
 
   return (
     <div className="h-dvh flex flex-col items-center justify-center">
       <div className="bg-white p-8 rounded-md shadow-lg">
         <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          Frontend Rocks
+          ROCKS
         </h1>
 
         <div className="flex justify-center space-x-4 mb-2">
@@ -21,14 +22,20 @@ export const App = () => {
           </a>
         </div>
 
-        <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
+        <h2 className="text-center font-bold text-xl mb-6">1 REACT = 1€</h2>
 
         <div className="flex flex-col items-center space-y-4">
           <button
             className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
             onClick={() => setCount((count) => count + 1)}
           >
-            Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
+            Hai guadagnato {count}€
+          </button>
+          <button
+          className="bg-red-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-red-600 transition-colors shadow"
+          onClick={() => setTitle("Charizzard")}
+          >
+            cambia titolo
           </button>
           <p className="text-center">
             Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
