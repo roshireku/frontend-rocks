2cb15e385b8b13a44c44e714f4b6260160f3555d
Yuki <146095835+roshireku@users.noreply.github.com>
2025-03-06 11:20:02 +0000

    change

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export const App = () => {
             className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
             onClick={() => setCount((count) => count + 1)}
           >
-            Hai guadagnato {count}€
+             Hai guadagnato {count}€
           </button>
           <button
           className="bg-red-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-red-600 transition-colors shadow"
